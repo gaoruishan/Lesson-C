14fea19c1b9d1fdc2340e6cd0e26321cb024391b
gaoruishan <grs0515@163.com>
2015-08-06 22:31:22 +0800

    git push origin master

--- L01HelloC/L01HelloC/main.c
+++ L01HelloC/L01HelloC/main.c
@@ -7,9 +7,14 @@
 //
 
 #include <stdio.h>
-
+ //说明：参数1:传入的数量；参数2:传入的值
+/*
+ 1,打印格式换行，输入的内容中的空格标志，有空格的内容用加“”；printf("%s\n",argv[i]);
+ */
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+      for (int i=0;i<argc;i++) {
+            printf("%s\n",argv[i]);
+      }
     return 0;
 }
