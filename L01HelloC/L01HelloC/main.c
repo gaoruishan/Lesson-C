4189c88d09d10e644601ab5538f09773e0977448
gaoruishan <grs0515@163.com>
2015-08-06 17:02:04 +0800

    git init

---
+++ L01HelloC/L01HelloC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  L01HelloC
+//
+//  Created by gaoruishan on 15/8/6.
+//  Copyright (c) 2015年 gaoruishan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
