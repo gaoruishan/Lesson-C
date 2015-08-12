c9af63622a07995b0493bd4c26434f4aafc1b0f4
gaoruishan <grs0515@163.com>
2015-08-12 22:53:20 +0800

    git push origin master

--- L01HelloC/L01HelloC/main.c
+++ L01HelloC/L01HelloC/main.c
@@ -7,14 +7,42 @@
 //
 
 #include <stdio.h>
+
  //说明：参数1:传入的数量；参数2:传入的值
 /*
- 1,打印格式换行，输入的内容中的空格标志，有空格的内容用加“”；printf("%s\n",argv[i]);
+ *1,打印格式换行，输入的内容中的空格标志，有空格的内容用加“”；printf("%s\n",argv[i]);
  */
 int main(int argc, const char * argv[]) {
     // insert code here...
+      
       for (int i=0;i<argc;i++) {
+            //print-输出 f－格式化，默认格式化输出  s 字符串
             printf("%s\n",argv[i]);
       }
+      
+      /*
+       *1, d或i 十进制整数（有符号）
+       *2, x   十六进制整数（无符号）
+       *3, f   十进制浮点小数，小写
+       */
+      printf("Hello,%d\n",100);
+      
+      //定义一个缓冲区，字符长度为100 －这是不安全的 当下面使用gets 时。
+      char buf[100];
+      
+      //get 是等待控制台输入，用户回车后 执行下面的代码。
+      gets(buf);
+      
+      //使用put－输出 s 字符串
+//      puts("hello C\n");
+      puts(buf);
+      
+      
+      int a;
+      //接受指定int 输入
+      scanf("%d",&a);
+      
+      printf("%d\n",a);
+      
     return 0;
 }
